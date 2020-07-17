de24cf08ff4006ec783458d3c51793373c973a8e
João Victor Rodrigues <joaovi.15.rodrigues@gmail.com>
2020-07-16 22:03:41 -0300

    Implementação - desafio 3.3 OK

---
+++ desafio-03.3/public/script.js
@@ -0,0 +1,28 @@
+const modalOverlay = document.querySelector('.modal_overlay')
+const cards = document.querySelectorAll('.curso-card')
+const modal = modalOverlay.querySelector(".modal")
+
+for (let card of cards) {
+    card.addEventListener('click', () => {
+        const Id = card.getAttribute('id')
+
+        window.location.href = `/course/${Id}`
+            //modalOverlay.classList.add('active')
+            // modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${Id}`
+    })
+}
+/*
+document.querySelector('.close-modal').addEventListener('click', () => {
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ''
+    modal.classList.remove('full')
+})
+
+document.querySelector('.fullscreen-modal').addEventListener('click', () => {
+
+    if (!modal.classList.contains('full')) {
+        modal.classList.add('full')
+    } else {
+        modal.classList.remove('full')
+    }
+})*/
