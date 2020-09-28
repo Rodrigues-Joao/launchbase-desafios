085c27e2439bfaa913da32054402b0b737d45caa
João Victor Rodrigues <joaovi.15.rodrigues@gmail.com>
2020-09-28 19:18:31 -0300

    Desafio 4.1 -Deve ser aplicado um background; -Deve ser utilizada a fonte Roboto; -Utilize o conceito de box-sizing e o seletor + para centralizar os seus links; -Utilize o after e o transition para aplicar um efeito visual nos links quando o mouse passar por cima. Please enter the commit message for your changes. Lines starting

---
+++ desafio-04.1/data.js
@@ -0,0 +1,41 @@
+module.exports = [{
+        id: "GykTLqODQuU",
+        title: "Formulário animado com JS puro e CSS animation",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "DDqRgIfHJeo",
+        title: "Por que o PLaystation 5 é assim?",
+        duration: "13 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "3suNo2fGJSw",
+        title: "A VELOCIDADE DOS PROCESSADORES E O GIGAHERTZ, QUE TROÇO É ESSE?",
+        duration: "11 min",
+        price: "Free"
+    },
+    {
+        id: "spqYMJe7XIs",
+        title: "10 PRINCÍPIOS de WARREN BUFFETT! | p/ investir MELHOR e ganhar mais dinheiro!",
+        duration: "14 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "UeOTzCvUDSE",
+        title: "TUDO SOBRE O NOVO IOS 14! (E TAMBÉM O QUE NÃO TE DISSERAM!) - HANDS ON BETA 1",
+        duration: "31 min",
+        price: "Free"
+    },
+    {
+        id: "dDWH3fELV6o",
+        title: "CAI BEM? NOVA ZONTES 310 ADV CAIRÁ BEM NO BRASIL? (Ela está pra chegar!) - Motorede",
+        duration: "10 min",
+        price: "Free",
+        featured: true
+    }
+
+]
---
+++ desafio-04.1/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const server = express()
+
+const route = require('./routes')
+const port = portNormalize(process.env.PORT || "4000");
+
+server.use(express.static('public'))
+server.use(route)
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+
+server.listen(port, () => {
+    console.log(`server is running in port ${port}`)
+})
+
+
+
+function portNormalize(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
